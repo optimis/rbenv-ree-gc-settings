6c1e4ef2eb456462670642d1f249d54a745c72ba
Umang Chouhan <uchouhan@optimiscorp.com>
2013-01-03 15:58:34 -0800

    Adding default attributes for REE GC settings.

---
+++ attributes/default.rb
@@ -0,0 +1,5 @@
+node['rbenv']['ruby_heap_min_slots'] = 1000000
+node['rbenv']['ruby_heap_slots_increment'] = 250000
+node['rbenv']['ruby_heap_slots_growth_factor'] = 1
+node['rbenv']['ruby_gc_malloc_limit'] = 80000000
+node['rbenv']['ruby_heap_free_min'] = 100000
