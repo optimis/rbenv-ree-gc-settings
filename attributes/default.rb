7491cbb6ae5c9d9f7a8e1959ca2c94571cdc5e6e
Umang Chouhan <uchouhan@optimiscorp.com>
2013-01-04 10:56:14 -0800

    Switching deprecated method to set node attributes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,7 @@
-node['rbenv']['ruby_heap_min_slots'] = 1000000
-node['rbenv']['ruby_heap_slots_increment'] = 250000
-node['rbenv']['ruby_heap_slots_growth_factor'] = 1
-node['rbenv']['ruby_gc_malloc_limit'] = 80000000
-node['rbenv']['ruby_heap_free_min'] = 100000
+set['rbenv'] ||= {}
+
+set['rbenv']['ruby_heap_min_slots'] = 1000000
+set['rbenv']['ruby_heap_slots_increment'] = 250000
+set['rbenv']['ruby_heap_slots_growth_factor'] = 1
+set['rbenv']['ruby_gc_malloc_limit'] = 80000000
+set['rbenv']['ruby_heap_free_min'] = 100000
